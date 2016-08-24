f9b4766943688deae29e9e91da56d95b52a140ec
Goyder <j_goyder@hotmail.com>
2016-08-24 19:54:52 +0800

    Update index, add static repos

---
+++ static/main.js
@@ -0,0 +1,16 @@
+function popup() {
+	window.alert("No, wait. No I haven't!");
+}
+
+list_of_quotes = [
+	"Oh baby, it's a wild world.",
+	"It's hard to get by, just upon a smile.",
+	"This sure is a quote from a list, buddy!",
+	"Thai, Vietnamese, Japanese, Sushi Train, buddy!",
+	"Freemasons run the country."
+	]
+
+function myFunction() {
+	document.getElementById("demo").innerHTML = list_of_quotes[Math.floor(Math.random() * list_of_quotes.length)];
+}
+
