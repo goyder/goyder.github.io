c54f96ef64bdcdf27ccc8b72ce1c915017252fba
goyder <j_goyder@hotmail.com>
2016-09-17 18:10:57 +0800

    Charts now work

--- static/chart.js
+++ static/chart.js
@@ -1,16 +1,17 @@
 $(function() {
-	var data = [1, 2, 3, 4, 5];
+	var data = [1, 2, 3, 4, 5, 6, 8];
 
-	var margin = {top: 20, right: 20, left: 40, bottom: 20},
+	var margin = {top: 20, right: 20, left: 70, bottom: 70},
 			width = parseInt(d3.select("div#chart").style("width")) - margin.left - margin.right,
 			height = 500 - margin.top - margin.bottom;
 
 	var x = d3.scaleBand()
 			.domain(data)
-			.range([0, width]);
+			.range([0, width])
+			.paddingInner(0.1);
 
 	var y = d3.scaleLinear()
-			.domain(data)
+			.domain([data[0], data[data.length - 1]])
 			.range([height, 0]);
 
 	var xAxis = d3.axisBottom()
@@ -41,11 +42,26 @@ $(function() {
 		.enter()
 		.append('rect')
 		.attr('class', 'bar')
+		.attr('fill', '#C02942')
 		.attr('x', function(d) { return x(d); })
-		.attr('width', 30) 
-		.attr('y', function(d) { return y(d); })
-		.attr('height', function(d) { return height - y(d); });
+		.attr('width', x.bandwidth()) 
+		.attr('y', function() { return height; })
+		.attr('height', 0)
+		.transition()
+			.delay(function(d,i) { return i*50; })
+			.duration(800)
+			.attr('y', function(d) { return y(d); })
+			.attr('height', function(d) { return height - y(d); });
 
+	svg.append("text")
+		.attr("text-anchor", "middle")
+		.attr("transform", "translate(" + (width/2) + "," + (height+(margin["bottom"]/2)) + ")")
+		.text("Dude, what.");
 
+	svg.append("text")
+		.attr("id", "y-axis")
+		.attr("text-anchor", "middle")
+		.attr("transform", "translate(" + (-margin["left"]/2) + "," + (height/2) + ") rotate(-90)")
+		.text("Seriously, what");
 
 });
