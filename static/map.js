c2d8243a7e6ef70ad0df9d1c2cea3d403c5d0e36
goyder <j_goyder@hotmail.com>
2016-10-08 13:26:39 +0800

    working tiles

--- static/map.js
+++ static/map.js
@@ -1,19 +1,13 @@
 $(function() {
-	// svg = d3.selectAll(".datamaps-subunits");
-	svg = d3.select("#chart");
-	var t = textures.lines().thicker().stroke("green").strokeWidth(2);
-	var texture_scroll = textures.lines().thicker().strokeWidth(3);
-	t.background("#AAAAAA");
-	texture_scroll.background("#AFAFAF");
-	svg.call(t);
-	svg.call(texture_scroll);
 
 	var worldmap = new Datamap({
 		element: document.getElementById("chart"),
 		height: 450,
+		/*	
 		fills: {
 			defaultFill: t.url()
 		},
+		*/
 		setProjection: function(element) {
 			var projection = d3.geo.mercator()
 				.center([9.993, 53.55])
@@ -27,13 +21,24 @@ $(function() {
 		},
 	});
 
+	/*
 	// Relocate the texture URLs?
 	datamap = document.getElementsByClassName("datamaps-subunits")[0];
 	defs = document.getElementsByTagName("defs");
 	datamap.appendChild(defs[0]);
 	datamap.appendChild(defs[0]);
+	*/
 
+	svg = d3.selectAll(".datamaps-subunits");
+	//svg = d3.select("#chart");
+	var t = textures.lines().thicker().stroke("green").strokeWidth(2);
+	var texture_scroll = textures.lines().thicker().strokeWidth(3);
+	t.background("#AAAAAA");
+	texture_scroll.background("#AFAFAF");
+	svg.call(t);
+	svg.call(texture_scroll);
+	
 	countries = d3.selectAll(".datamaps-subunit");
-	// countries.style("fill", t.url());
+	countries.style("fill", t.url());
 	countries.style("stroke", "000000");
 });
