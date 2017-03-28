f86b7b82e4cbc1517338f0703b21e050e63c0334
goyder <j_goyder@hotmail.com>
2017-03-28 08:02:42 +0800

    Add data file

---
+++ static/data.js
@@ -0,0 +1,70 @@
+data=`Index,Datetime,Onshore,Location
+0,2017-03-15,0,on the Nganhurra.
+1,2017-03-16,0,on the Nganhurra.
+2,2017-03-17,0,on the Nganhurra.
+3,2017-03-18,0,on the Nganhurra.
+4,2017-03-19,0,on the Nganhurra.
+5,2017-03-20,0,on the Nganhurra.
+6,2017-03-21,0,on the Nganhurra.
+7,2017-03-22,0,on the Nganhurra.
+8,2017-03-23,0,on the Nganhurra.
+9,2017-03-24,0,on the Nganhurra.
+10,2017-03-25,0,on the Nganhurra.
+11,2017-03-26,0,on the Nganhurra.
+12,2017-03-27,0.5,
+13,2017-03-28,1,
+14,2017-03-29,1,
+15,2017-03-30,1,
+16,2017-03-31,1,
+17,2017-04-01,1,
+18,2017-04-02,1,
+19,2017-04-03,1,douth.
+20,2017-04-04,1,douth.
+21,2017-04-05,1,douth.
+22,2017-04-06,1,douth.
+23,2017-04-07,1,douth.
+24,2017-04-08,1,
+25,2017-04-09,1,
+26,2017-04-10,0,on the Nganhurra.
+27,2017-04-11,0,on the Nganhurra.
+28,2017-04-12,0,on the Nganhurra.
+29,2017-04-13,0,on the Nganhurra.
+30,2017-04-14,0,on the Nganhurra.
+31,2017-04-15,0,on the Nganhurra.
+32,2017-04-16,0,on the Nganhurra.
+33,2017-04-17,0,on the Nganhurra.
+34,2017-04-18,0,on the Nganhurra.
+35,2017-04-19,0,on the Nganhurra.
+36,2017-04-20,0,on the Nganhurra.
+37,2017-04-21,0,on the Nganhurra.
+38,2017-04-22,0,on the Nganhurra.
+39,2017-04-23,0,on the Nganhurra.
+40,2017-04-24,0.5,
+41,2017-04-25,1,
+42,2017-04-26,1,
+43,2017-04-27,1,
+44,2017-04-28,1,
+45,2017-04-29,1,
+46,2017-04-30,1,
+47,2017-05-01,1,
+48,2017-05-02,1,
+49,2017-05-03,1,
+50,2017-05-04,1,
+51,2017-05-05,1,
+52,2017-05-06,1,
+53,2017-05-07,1,
+54,2017-05-08,0,on the Ngujima-Yin.
+55,2017-05-09,0,on the Ngujima-Yin.
+56,2017-05-10,0,on the Ngujima-Yin.
+57,2017-05-11,0,on the Ngujima-Yin.
+58,2017-05-12,0,on the Ngujima-Yin.
+59,2017-05-13,0,on the Ngujima-Yin.
+60,2017-05-14,0,on the Ngujima-Yin.
+61,2017-05-15,0,on the Ngujima-Yin.
+62,2017-05-16,0,on the Ngujima-Yin.
+63,2017-05-17,0,on the Ngujima-Yin.
+64,2017-05-18,0,on the Ngujima-Yin.
+65,2017-05-19,0,on the Ngujima-Yin.
+66,2017-05-20,0,on the Ngujima-Yin.
+67,2017-05-21,0,on the Ngujima-Yin.
+68,2017-05-22,0.5,`
